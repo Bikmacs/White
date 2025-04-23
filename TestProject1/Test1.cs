4bf9fd62fc3b1b50afcc3325e052c00928847243
Bikmacs <macsim.shmelev@yandex.ru>
2025-04-23 09:16:54 +0500

    raboty

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,49 @@
+﻿namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestClass]
+        public class PrimeCheckerTests
+        {
+            [TestMethod]
+            public void Test_IsPrime_LessThanTwo()
+            {
+                Assert.IsFalse(PrimeChecker.IsPrime(1));
+                Assert.IsFalse(PrimeChecker.IsPrime(0));
+                Assert.IsFalse(PrimeChecker.IsPrime(-5));
+            }
+
+            [TestMethod]
+            public void Test_IsPrime_PrimeNumbers()
+            {
+                Assert.IsTrue(PrimeChecker.IsPrime(2));
+                Assert.IsTrue(PrimeChecker.IsPrime(3));
+                Assert.IsTrue(PrimeChecker.IsPrime(5));
+                Assert.IsTrue(PrimeChecker.IsPrime(7));
+            }
+
+            [TestMethod]
+            public void Test_IsPrime_NonPrimeNumbers()
+            {
+                Assert.IsFalse(PrimeChecker.IsPrime(4));
+                Assert.IsFalse(PrimeChecker.IsPrime(6));
+                Assert.IsFalse(PrimeChecker.IsPrime(8));
+                Assert.IsFalse(PrimeChecker.IsPrime(9));
+            }
+
+            [TestMethod]
+            public void Test_IsPrime_LargePrimeNumber()
+            {
+                Assert.IsTrue(PrimeChecker.IsPrime(29));
+            }
+
+            [TestMethod]
+            public void Test_IsPrime_LargeNonPrimeNumber()
+            {
+                Assert.IsFalse(PrimeChecker.IsPrime(100));
+            }
+        }
+    }
+    
+}
---
+++ console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class PrimeChecker
+{
+    public static bool IsPrime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
